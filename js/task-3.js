ca305dbe6859ea9937d64a3ce67d5f1383ad01df
L-y-u-d-m-y-l-a <lyudmyla.k@hotmail.com>
2024-04-07 10:19:01 +0100

    result_js_hw_02_upd

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,6 @@
 function checkForSpam(message) {
-    const lowerCaseMessage = message.toLowerCase(); 
-
-    if (lowerCaseMessage.includes('spam') || lowerCaseMessage.includes('sale')) {
-        return true;
-    } else {
-        return false;
-    }
+    const lowerCaseMessage = message.toLowerCase();
+    return lowerCaseMessage.includes('spam') || lowerCaseMessage.includes('sale');
 }
 
 
